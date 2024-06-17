27c48b0cb91a3bed5cd89dfbdf39d8f22ff92ab0
EscobarMariaSol <mariasolescobar21@gmail.com>
2024-06-17 20:43:01 -0300

    Agrego los spinners de la reserva de turno

--- app/src/main/java/com/sepriceapp/ReserveAppointmentActivity.kt
+++ app/src/main/java/com/sepriceapp/ReserveAppointmentActivity.kt
@@ -3,7 +3,9 @@ package com.sepriceapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,7 +16,61 @@ class ReserveAppointmentActivity : AppCompatActivity(){
         setContentView(R.layout.reserve_appointments)
         enableEdgeToEdge()
 
-       // Go back button
+        //Spinners
+
+        // Especialidades
+        val items_especialidades = resources.getStringArray(R.array.dropdown_especialidades)
+
+        val especialidades = findViewById<Spinner>(R.id.spinner_especialidades)
+        if (especialidades != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, items_especialidades
+            )
+            especialidades.adapter = adapter
+        }
+
+        // Profesionales
+        val items_profesionales = resources.getStringArray(R.array.dropdown_profesionales)
+        val profesionales = findViewById<Spinner>(R.id.spinner_profesionales)
+        if (profesionales != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, items_profesionales
+            )
+            profesionales.adapter = adapter
+        }
+
+        // Fechas
+        val items_fechas = resources.getStringArray(R.array.dropdown_fechas)
+        val fechas = findViewById<Spinner>(R.id.spinner_fechas)
+        if (fechas != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, items_fechas
+            )
+            fechas.adapter = adapter
+        }
+
+        // Horarios
+        val items_horarios = resources.getStringArray(R.array.dropdown_horarios)
+        val horarios = findViewById<Spinner>(R.id.spinner_horarios)
+        if (horarios != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, items_horarios
+            )
+            horarios.adapter = adapter
+        }
+
+        // confirm button
+        val confirmButton = findViewById<Button>(R.id.confirmButton)
+        val confirmIntent = Intent(this, MyAppointmentsActivity::class.java)
+        confirmButton.setOnClickListener {
+            startActivity(confirmIntent)
+        }
+
+        // Go back button
         val goBackButton = findViewById<Button>(R.id.goBackButton)
         val goBackIntent = Intent(this, MainMenuActivity::class.java)
         goBackButton.setOnClickListener {
